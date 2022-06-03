4baa9014c85a60e0fcaa8007e2bc90d2f2335236
annk1995 <kabuikariuki052@gmail.com>
2022-06-03 14:59:26 +0300

    initial commit

--- form/models.py
+++ form/models.py
@@ -1,13 +1,4 @@
-# from django.db import models
-
-
-# # Create your models here.
-# class Users (models.Model):
-#     name=models.CharField(max_length=50,null=False,blank=False)
-    
-#     def __str__(self):
-#         return self.name
-    
+ 
 from django.db import models
 from django.contrib.auth.models import User
 
